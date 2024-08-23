34dd698a1463db82b4cbdd03ffa69f8e8f723049
Abega1642 <hei.abega.2@gmail.com>
2024-08-23 10:09:41 +0300

    feat: add lombok annotations to RoomReview class

--- src/main/java/dev/razafindratelo/reviews/RoomReview.java
+++ src/main/java/dev/razafindratelo/reviews/RoomReview.java
@@ -2,9 +2,18 @@ package dev.razafindratelo.reviews;
 
 import dev.razafindratelo.customer.Customer;
 import dev.razafindratelo.places.hotel.Room;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public class RoomReview extends Review {
     private Room commentedRoom;
+
     public RoomReview(String content, Customer author, Room commentedRoom) {
         super(content, author);
         this.commentedRoom = commentedRoom;
