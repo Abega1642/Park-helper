428777a5bdd9a56a985738b756e819fa672fcf24
Abega1642 <hei.abega.2@gmail.com>
2024-08-23 10:10:50 +0300

    feat: add list of all reveiws attribute to Place class

--- src/main/java/dev/razafindratelo/places/Place.java
+++ src/main/java/dev/razafindratelo/places/Place.java
@@ -1,19 +1,24 @@
 package dev.razafindratelo.places;
 
 
+import dev.razafindratelo.reviews.PlaceReview;
 import lombok.Data;
-
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Data
 public abstract class Place {
     private String name;
     private Point2D coordinates;
     private String description;
+    private List<PlaceReview> reviews;
 
     public Place(String name, double latitude, double longitude,String description) {
         this.name = name;
         this.coordinates = new Point2D.Double(latitude, longitude);
         this.description = description;
+        this.reviews = new ArrayList<>();
     }
 }
