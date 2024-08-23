1e309f04f7ed8ee6436e5df1750a72d984dba008
Abega1642 <hei.abega.2@gmail.com>
2024-08-23 09:40:34 +0300

    feat: add customer package which contains the Customer class

--- src/main/java/dev/razafindratelo/customer/Customer.java
+++ src/main/java/dev/razafindratelo/customer/Customer.java
@@ -1,4 +1,18 @@
-package dev.razafindratelo.user;
+package dev.razafindratelo.customer;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class Customer {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+
 }
